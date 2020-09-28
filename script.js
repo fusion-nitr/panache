9b86276d4378058ce594276b7dbd54d34d2334c3
Amlana-P <50160346+Amlana-P@users.noreply.github.com>
2020-09-28 09:25:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Sep 28, 2020 00:00:00').getTime(),
+let countDown = new Date('Sep 28, 2020 06:00:00').getTime(),
     x = setInterval(function() {    
 
       let now = new Date().getTime(),
@@ -13,11 +13,5 @@ let countDown = new Date('Sep 28, 2020 00:00:00').getTime(),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
 
     }, second)
